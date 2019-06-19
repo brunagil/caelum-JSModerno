781bdc09f8c480c7afc25244ff81b245a204680d
Bruna Gil <brunagil.eppen@gmail.com>
2019-06-18 22:31:32 -0300

    create IIFE function and discuss about strict and module pattern

--- scripts/aceitouAnteriormente.js
+++ scripts/aceitouAnteriormente.js
@@ -1,13 +1,32 @@
+// Função Anônima Imediatamente Invocada (IIFE)
+// IIFE 
+// função anônima protegida; é o único projeto
+//de criar escopo < ECMA2015 ES6
 
-const aceitouAnteriormente = localStorage.getItem("aceitouSalvar")
+// O modo estrito/strict mode aponta os erros de declaração
+// de um código e faz com que a dev saiba do problema
+// ele não trava a operação mas aponta os erros 
 
-if(!aceitouAnteriormente) {
-    //não tem problema essa variável ter o mesmo nome que tem no pedePaginaInicial,
-    //pois essa variável aqui está apenas e somente no escopo do if 
-    const aceitouSalvar = confirm('Você aceita que a gente salve suas informações?')
-    if (!aceitouSalvar) {
-        alert('Você pode mudar isso na página de configurações') 
-    }
+// MODULE PATTERN - Código tem um IIFE e um executável no final e cria um escopo estrito
+
+(function () {
+
+    "use strict"
+    var aceitouAnteriormente = localStorage.getItem("aceitouSalvar")
     
-    localStorage.setItem('aceitouSalvar', aceitouSalvar)
-}
+    if(!aceitouAnteriormente) {
+        //não tem problema essa variável ter o mesmo nome que tem no pedePaginaInicial,
+        //pois essa variável aqui está apenas e somente no escopo do if 
+        const aceitouSalvar = confirm('Você aceita que a gente salve suas informações?')
+        if (!aceitouSalvar) {
+            alert('Você pode mudar isso na página de configurações') 
+        }
+        
+        localStorage.setItem('aceitouSalvar', aceitouSalvar)
+    }
+
+    alert('oi, eu sou uma função anônima')
+//invoca a função imediatamente no final dela
+})()
+
+//dá pra escrever essa function também utilizando a arrow function (es6) . Ex: ;(() =>
