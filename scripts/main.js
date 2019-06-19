903e78f1920b340a281aea291a859f61957c852c
Bruna Gil <brunagil.eppen@gmail.com>
2019-06-19 19:23:44 -0300

    create main js to keep all scripts;update scripts with export and import

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+import './termosDeUso'
+import './pedePaginaInicial'
--- scripts/pedePaginaInicial.js
+++ scripts/pedePaginaInicial.js
@@ -1,22 +1,25 @@
-// declaração de variável iria no topo?
-// Hoisting-Içamento
-// var paginaInicial = undefined (recebe um valor, e esse valor é undefined)
+//--declaração de variável iria no topo?
+//--Hoisting-Içamento
+//--var paginaInicial = undefined (recebe um valor, e esse valor é undefined)
 
-// const paginaInicial = prompt('oi')
+//--const paginaInicial = prompt('oi')
 
-// é LET pois ele vai ser concatenado no if abaixo dele, ganhando outro valor
+//--é LET pois ele vai ser concatenado no if abaixo dele, ganhando outro valor
 
-//conversão automática de uma variável de um tipo pra outro
-//exemplo: paginaInicial citada como false e por dentro
+//--conversão automática de uma variável de um tipo pra outro
+//--exemplo: paginaInicial citada como false e por dentro
 
-// if(paginaInicial !== null && paginaInicial !== false)
+// --if(paginaInicial !== null && paginaInicial !== false)
 
-// Type Coercing - Conversão que o JS faz de forma automática, por baixo dos 'panos'
-//quando ele converte uma variável para outro tipo para fazer o script rodar
+// --Type Coercing - Conversão que o JS faz de forma automática, por baixo dos 'panos'
+//--quando ele converte uma variável para outro tipo para fazer o script rodar
 
-const aceitouSalvar = JSON.parse(localStorage.getItem('aceitouSalvar'))   
 
-if(aceitouSalvar) {
+//--const aceitouSalvar = JSON.parse(localStorage.getItem('aceitouSalvar'))   
+
+import aceitouSalvar from './aceitouAnteriormente.js'
+
+if(aceitouSalvar === null || aceitouSalvar === true) {
 
     let paginaInicial = localStorage.getItem('paginaInicial')
 
@@ -29,7 +32,7 @@ if(aceitouSalvar) {
             paginaInicial.substring(0, 7) !== 'http://' &&
             paginaInicial.substring(0, 8) !== 'https://'
         ) {
-            //Assigment-Atribuição
+            //---Assigment-Atribuição
             paginaInicial = 'http://' + paginaInicial
         }
     
@@ -39,10 +42,6 @@ if(aceitouSalvar) {
         localStorage.setItem('paginaInicial', paginaInicial)
     }
 }
-
-
-
-
 
 
     //var paginaInicial = "http://google.com"
