3403d14b85289575ca06110a40017334b2abe0aa
Bruna Gil <brunagil.eppen@gmail.com>
2019-06-25 21:07:57 -0300

    create historico to organize coding and work with modules

---
+++ scripts/navegacao/historico.js
@@ -0,0 +1,26 @@
+const listaSites = []
+let posicao = -1
+
+export function adiciona(endereco)
+if (endereco !== listaSites[posicao]) {
+    listaSites.splice(posicao + 1)
+    listaSites.push(endereco)
+    posicao++
+}
+
+export function volta() {
+    const isPrimeiraPosicao = posicao === 0
+    if (listaSites.length !== 1 && !isPrimeiraPosicao) {
+        posicao = posicao - 1
+        return listaSites[posicao]
+    }
+}
+
+
+export function avanca() {
+    const isUltimaPosicao = posicao === listaSites.length - 1
+    if (listaSites.length !== 1 && !isUltimaPosicao) {
+        posicao = posicao + 1
+        return listaSites[posicao]
+    }
+}
--- scripts/navegacao/paginaAtual.js
+++ scripts/navegacao/paginaAtual.js
@@ -1,5 +1,6 @@
 import * as storagePaginaInicial from '/scripts/storage/paginaInicial.js'
 import { formataEndereco } from '/scripts/endereco/formataEndereco.js'
+import { carregar } from '/scripts/navegacao/carregarHome.js'
 
 const paginaAtual = sessionStorage.getItem('paginaAtual')
 //PASSO A PASSO: pegar página atual do localstorage
@@ -10,40 +11,32 @@ const paginaPraCarregar = paginaAtual !== null
     ? paginaAtual
     : storagePaginaInicial.paginaInicial
 
-    const enderecoCompleto = formataEndereco(paginaPraCarregar)
+const enderecoCompleto = formataEndereco(paginaPraCarregar)
 
     $janelaPrincipal.src = enderecoCompleto
     $inputEndereco.value = enderecoCompleto
 
-    $janelaPrincipal.addEventListener('load', salvaPaginaAtual)
-// $janelaPrincipal.onload = salvaPaginaAtual
+    //Closures
+    $janelaPrincipal.addEventListener('load', function salvaPaginaAtual() {
+        // $janelaPrincipal.onload = salvaPaginaAtual
+        const endereco = $janelaPrincipal.contentWindow.location.href
+        sessionStorage.setItem('paginaAtual', endereco)
+    })
 
-    function salvaPaginaAtual() {
-        const enderecoAcessado = $janelaPrincipal.contentWindow.location.href
-        sessionStorage.setItem('paginaAtual', enderecoAcessado)
+    $janelaPrincipal.addEventListener('load', function salvaPaginaAtual() {
+    const endereco = $janelaPrincipal.contentWindow.location.href
         //coloca os sites acessados na array de sites
-        listaSites.push(enderecoAcessado)
-    }
+
+    })
 
 // função BOTÃO VOLTAR PARA A PÁGINA ANTERIOR
 
-let posicao = -1
 let listaSites = [] 
+let posicao = -1
 
 $botaoVoltar.addEventListener('click', function() {
-    if(listaSites.length !== 1) {
-        posicao--
-        carregar(
-            listaSites[posicao] //acessar as posições dos sites na lista
-            )
-    }
+
 })
 
-$botaoAvancar.addEventListener('click', function() {
-    if(listaSites.length !== 1) {
-        posicao++
-        carregar(
-            listaSites[posicao] //acessar as posições dos sites na lista
-            )
-    }
+
 })
