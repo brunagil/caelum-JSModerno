71f49811dec6d9567edfe37d89c96cd535d4d546
Bruna Gil <brunagil.eppen@gmail.com>
2019-06-26 22:42:46 -0300

    refactor criaEndereco and delete extra if

--- scripts/endereco/criaEndereco.js
+++ scripts/endereco/criaEndereco.js
@@ -1,25 +1,21 @@
 // função construtora
 // função factory
-// fábrica de objetos 
+// fábrica de objetos - class
 function criaEndereco(endereco) {
     const url = new URL(endereco) 
 
     let enderecoCompleto
     if(url.hostname === 'localhost') {
-        enderecoCompleto = url.pathname.replace("/", "")
+        const paginaLocal = url.pathname.replace("/", "")
+        enderecoCompleto = paginaLocal
+        enderecoResumido = paginaLocal
     } else {
         // colocando apenas 'url' também funciona
         // pois na hora do type coercing a função toString é chamada por baixo dos panos
         enderecoCompleto = url.toString()
-    }
-
-    let enderecoResumido
-    if(url.hostname === 'localhost') {
-        enderecoResumido = url.pathname.replace("/", "")
-    } else {
         enderecoResumido = url.hostname
     }
-
+    
     return {
         urlCompleta: enderecoCompleto,
         urlResumida: enderecoResumido
